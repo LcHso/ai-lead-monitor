6ad642886b2e498ccc0120eab53fa54faf08bd8c
LcHso <51322139+LcHso@users.noreply.github.com>
2026-02-25 09:26:42 +0800

    Update config.py
    增加钉钉输出名单上限到20

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(BaseModel):
     dashscope_api_key: str = os.environ.get("DASHSCOPE_API_KEY")
     dingtalk_webhook: str = os.environ.get("DINGTALK_WEBHOOK")
     country: str = "CN"
-    top_n: int = 10
+    top_n: int = 20
     keywords: list = ["AI 陪伴", "虚拟恋人", "AI 聊天", "角色扮演", "智能女友", "AI 朋友", "情感倾诉", "树洞","AI聊天", "AI角色", "AI男友", "AI女友", "虚拟人", "AI对话", "Character", "Roleplay"]
-    exclude_sellers: list = ["Tencent", "Baidu", "Alibaba", "NetEase"]
+    : list = ["Tencent", "Baidu", "Alibaba", "NetEase"]
 
     class Config:
         populate_by_name = True
