e180221ec7b809a531a539d1451263fd3e680088
LcHso <51322139+LcHso@users.noreply.github.com>
2026-02-25 09:27:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(BaseModel):
     country: str = "CN"
     top_n: int = 20
     keywords: list = ["AI 陪伴", "虚拟恋人", "AI 聊天", "角色扮演", "智能女友", "AI 朋友", "情感倾诉", "树洞","AI聊天", "AI角色", "AI男友", "AI女友", "虚拟人", "AI对话", "Character", "Roleplay"]
-    : list = ["Tencent", "Baidu", "Alibaba", "NetEase"]
+    exclude_sellers: list = ["Tencent", "Baidu", "Alibaba", "NetEase"]
 
     class Config:
         populate_by_name = True
